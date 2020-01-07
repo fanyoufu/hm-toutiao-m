fbd8d7f6918b337d2246232650426a2b253c036a
fanyoufu <fanyoufu@outlook.com>
2020-01-07 16:12:49 +0800

    初始化结构

---
+++ src/views/video/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+  export default {
+    name: '',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
