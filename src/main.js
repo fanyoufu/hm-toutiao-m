00c9660ce76a412960a5f69afd575ad62f6d951c
fanyoufu <fanyoufu@outlook.com>
2020-01-07 17:30:31 +0800

    添加rem适配

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import store from './store'
 import Button from 'vant/lib/button'
 import 'vant/lib/button/style'
 
+// 手淘团队的方案
+// rem 的基准值是 屏宽的1/10
+import 'amfe-flexible'
 Vue.use(Button)
 Vue.config.productionTip = false
 
